8c6104cb44860c39c4f4b502614a2c44f5888b49
Steven Wu <steven.wu@flinders.edu.au>
2017-04-07 16:30:17 +0930

    Use Express built-in server.

--- app.js
+++ app.js
@@ -3,8 +3,9 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-
 const app = express();
+
+app.listen(8000);
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,7 +14,7 @@ app.use(cors());
 require('./server/routes')(app);
 app.get('*', function(req, res) {
   res.status(200).send({
-    message: 'Welcome to Daneland',
+    message: 'Welcome to Quit Smoking!',
   });
 });
 

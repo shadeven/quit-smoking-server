56f6e87d1ee1d8d9855921ed5755237b7bc496cf
Steven Wu <steven.wu@flinders.edu.au>
2017-04-07 10:44:44 +0930

    Enable CORS.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 
 const app = express();
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 require('./server/routes')(app);
 app.get('*', function(req, res) {
